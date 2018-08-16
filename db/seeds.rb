8e864004652f116164e5fce37d95052a47b4d43f
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2018-08-16 01:36:40 -0300

    Added a new gem and Populating database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'Populando banco de dados...'
+Kind.create!([
+  {description: "Amigo"},
+  {description: "Colega"},
+  {description: "Familía"}
+  ])
+
+10.times do |contact|
+  Contact.create!(
+    name: Faker::Name.name ,
+    email: Faker::Internet.email,
+    kind_id: Kind.all.sample,
+    rmk: Faker::Lorem.paragraph([1,2,3,4,5].sample)
+  )
+end
+puts 'Populando banco de dados...[OK]'

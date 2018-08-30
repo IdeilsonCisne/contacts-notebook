06284fdaae6e321c66fdcce29e6f93a918f4c5d5
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2018-08-30 17:07:11 -0300

    Add complex form with nested attributes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,6 +15,7 @@ class ContactsController < ApplicationController
   # GET /contacts/new
   def new
     @contact = Contact.new
+    @contact.build_address
     options_for_select
   end
 
@@ -74,6 +75,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :kind_id, :rmk)
+      params.require(:contact).permit(:name, :email, :kind_id, :rmk, address_attributes: [:street, :city, :state])
     end
 end

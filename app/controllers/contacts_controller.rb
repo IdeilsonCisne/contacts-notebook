7920c319edd17cd2ed086d142f0ceef4588067fa
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2018-10-29 12:10:51 -0300

    Added a new erros messeges and added filters in controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :set_options_for_select, only: [:new, :edit, :update, :create]
 
   # GET /contacts
   # GET /contacts.json
@@ -16,12 +17,10 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
     @contact.build_address
-    options_for_select
   end
 
   # GET /contacts/1/edit
   def edit
-    options_for_select
   end
 
   # POST /contacts
@@ -65,7 +64,7 @@ class ContactsController < ApplicationController
   end
 
   private
-    def options_for_select
+    def set_options_for_select
       @kind_options_for_select = Kind.all
     end
     # Use callbacks to share common setup or constraints between actions.
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,6 @@ class Contact < ActiveRecord::Base
   accepts_nested_attributes_for :address
   accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
 
-  # validates :name, presence: true
-  # validates :email, presence: true, length: { minimum: 5 }
+  validates :name, presence: true
+  validates :email, presence: true, length: { minimum: 5 }
 end
